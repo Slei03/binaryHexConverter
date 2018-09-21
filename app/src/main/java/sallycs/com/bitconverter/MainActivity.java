9d23d9a62d325df0be1bc7398e6e6d5a5d2e22f8
Asdfghjkloiuyt <sallycslei@gmail.com>
2018-09-21 07:34:00 -0700

    Initial commit

---
+++ app/src/main/java/sallycs/com/bitconverter/MainActivity.java
@@ -0,0 +1,28 @@
+package sallycs.com.bitconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.decInput);
+                int bit = Integer.parseInt(input.getText().toString());
+
+                
+            }
+        });
+    }
+}
