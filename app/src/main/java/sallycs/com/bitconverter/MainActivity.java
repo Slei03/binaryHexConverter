468518f07d2da3100bce5d1a321951dcf39d1a38
Asdfghjkloiuyt <sallycslei@gmail.com>
2018-10-04 21:01:20 -0700

    first commit

--- app/src/main/java/sallycs/com/bitconverter/MainActivity.java
+++ app/src/main/java/sallycs/com/bitconverter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +20,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText input = findViewById(R.id.decInput);
-                int bit = Integer.parseInt(input.getText().toString());
+                String decTxt = input.getText().toString();
+                TextView message = findViewById(R.id.msg);
+                TextView binary = findViewById(R.id.bitVal);
+                TextView hexMsg = findViewById(R.id.hexMsg);
+                TextView hex = findViewById(R.id.hexVal);
 
-                
+                message.setText("");
+                binary.setText("");
+                hexMsg.setText("");
+                hex.setText("");
+
+
+                if(decTxt.length()<0) {
+                    message.setText("Input a number");
+                }
+                else{
+                    int dec = Integer.parseInt(decTxt);
+                    input.getText().clear();
+
+                    if(dec>255){
+                        message.setText("Input a number from 0 to 255");
+                    }
+                    else{
+                        message.setText("Binary Value of " + dec +":");
+                        binary.setText(Integer.toBinaryString(dec));
+
+                        hexMsg.setText("Hexadecimal Value of " + dec +":");
+                        hex.setText(Integer.toHexString(dec));
+                    }
+                }
+
+
+
             }
         });
     }
